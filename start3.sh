7718fe251094edfbb18f8c0313c7cc7edcf7ca27
Chalapathi <princechalapathi16@gmail.com>
2017-11-27 17:07:34 +0000

    first commit

---
+++ start3.sh
@@ -0,0 +1,4 @@
+PATH=${PATH}:~/aarch64-linux-android-5.3/bin
+export ARCH=arm64
+make CP8676_I02_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
+make -j5 ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
